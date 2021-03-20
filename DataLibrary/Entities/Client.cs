a83f07fd2cd2dd2ac4376f38132ba62476ae4f3a
Tonari239 <vanina103@abv.bg>
2021-03-20 17:16:55 +0200

    Data Layer
    Entities, CRUD repositories, Db context class

---
+++ DataLibrary/Entities/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLibrary.Entities
+{
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsAdult { get; set; }
+
+        //Navigation Properties
+        
+        public virtual Reservation Reservation { get; set; }
+    }
+}
---
+++ DataLibrary/Repositories/CRUDRepository.cs
@@ -0,0 +1,62 @@
+﻿using DataLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataLibrary.Repositories
+{
+    public abstract class CRUDRepository<T>  where T : class
+    {
+
+        private readonly HotelDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public CRUDRepository(HotelDbContext Context, DbSet<T> DBSet)
+        {
+            this._context = Context;
+            this._dbSet = DBSet;
+        }
+
+        public void Add(T item)
+        {
+            _dbSet.Add(item);
+            
+        }
+        public void Remove(T item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+        
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public int Count()
+        {
+            return _dbSet.Count();
+        }
+
+         public int Count(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Count(expression);
+        }
+
+        public IQueryable Get(Expression<Func<T,bool>> expression)
+        {
+            return _dbSet.Where(expression).AsQueryable();
+        }
+
+        public T GetById(int id)
+        {
+           return _dbSet.Find(id);
+        }
+
+
+    }
+}
