360b57fa4deeba5c8598354e3eee7e6e6e7f5edc
Strahil Peykov <47077761+StrahilPeykov@users.noreply.github.com>
2021-04-17 21:20:25 +0300

    Validate

--- DataLibrary/Entities/GlobalVar.cs
+++ DataLibrary/Entities/GlobalVar.cs
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
-﻿namespace DataLibrary.Entities
-{
-    public static class GlobalVar
-    {
-        public const int InlcludedBreakfastExtraBillPercentage = 5;
 
-        public const int AllInclusiveExtraBillPercentage = 10;
-
-        public const int DefaultReservationHourStart = 12;
-=======
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataLibrary.Entities
 {
@@ -24,7 +16,6 @@ namespace DataLibrary.Entities
 
         public const int DefaultReservationHourStart = 12;
 
->>>>>>> Stashed changes
         public static int AmountOfElementsDisplayedPerPage { set; get; } = 10;
 
         public static int LoggedOnUserId { set; get; } = -1;
@@ -34,12 +25,7 @@ namespace DataLibrary.Entities
             DefaultUser
         }
         public static UserRights LoggedOnUserRights { set; get; } = UserRights.DefaultUser;
-<<<<<<< Updated upstream
-    }
-}
-=======
 
 
     }
 }
->>>>>>> Stashed changes
